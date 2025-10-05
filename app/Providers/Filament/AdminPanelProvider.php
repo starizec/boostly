d8a6a071fb149122d9a5ac83d78b82ba7860bbe3
Davor <davor@emedia.hr>
2025-10-05 16:11:37 +0200

    Dodao novu rutu

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -25,7 +25,7 @@ class AdminPanelProvider extends PanelProvider
         return $panel
             ->default()
             ->id('admin')
-            ->path('')
+            ->path('/boostadmin')
             ->login()
             ->defaultThemeMode(ThemeMode::Light)
             ->darkMode(false)
